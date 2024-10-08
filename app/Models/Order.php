a26eb93b39f1fb235b6dfecda13eb23be7086f52
Fedde <feddevangils@gmail.com>
2024-10-08 09:31:51 +0200

    add migrations + seeders

---
+++ app/Models/Order.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    public function table() {
+        return $this->belongsTo(Table::class, 'table_id');
+    }
+
+    public function products() {
+        return $this->belongsToMany(Product::class, 'order_product', 'order_id', 'product_id')->withPivot('quantity');
+    }
+}
---
+++ database/migrations/2024_10_08_071843_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->text('ingredients')->nullable();
+            $table->decimal('price', 8, 2);
+            $table->boolean('showOnMenu')->default(true);
+            $table->enum('type', ['food', 'drink']);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
